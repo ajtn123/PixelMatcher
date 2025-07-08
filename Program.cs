9ec2cf28f738e2ae1f4bf9a9a4a0244b5936fbdc
ajtn123 <Ajitani-Hifumi@outlook.com>
2025-07-08 18:59:34 +0800

    add options

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Data.Common;
+using System.Diagnostics;
 using ImageMagick;
 using PixelMatcher;
 
@@ -24,6 +25,21 @@ var diffImages = results.Select(r => r.DiffImage).ToArray();
 stopwatch.Stop();
 Console.WriteLine($"Time Used: {stopwatch.Elapsed.TotalMilliseconds} ms");
 
+Console.WriteLine("Save Diff Images? (Y/n)");
+if (Console.ReadLine()?.Contains('n') ?? false) return;
+
+var formats = MagickNET.SupportedFormats.Where(x => x.SupportsWriting).Select(x => x.Format.ToString().ToLower());
+Console.WriteLine($"Format? ({formats.Aggregate((x, y) => x + "/" + y).Replace("/png/", "/").Insert(0, "PNG/")})");
+var format = Console.ReadLine()?.Trim('.').ToLower();
+if (!formats.Contains(format)) format = "png";
+
+Console.WriteLine("Quality? (75/0-100)");
+bool isQualitySet = uint.TryParse(Console.ReadLine(), out uint quality);
+
 var opID = DateTime.Now.Millisecond;
 for (int i = 0; i < results.Length; i++)
-    diffImages[i].Write($"diff-{opID,3:000}-{i}.png");
+{
+    if (isQualitySet) diffImages[i].Quality = Math.Clamp(quality, 0, 100);
+    Console.WriteLine($"Writing 'diff-{opID,3:000}-{i}.{format}' with quality of {diffImages[i].Quality}");
+    diffImages[i].Write($"diff-{opID,3:000}-{i}.{format}");
+}
